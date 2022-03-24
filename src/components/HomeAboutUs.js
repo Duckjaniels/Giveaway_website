2fc4195aaf4d0f590f273deffe2114e676f10e3a
Maksym Kaczorowski <maksym73@gmail.com>
2022-03-24 19:19:53 +0100

    Home About Us section structure complete

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -1,8 +1,20 @@
 import React from 'react';
-
+import '../scss/Home/HomeAboutUs.scss'
+import Decoration from "../assets/Decoration.svg";
+import Signature from "../assets/Signature.svg";
+import People from "../assets/People.jpg"
 
 
 const HomeAboutUs = () => (
-    <div className="columns_section" title="section3" id="section3">about us</div>
+    <div className="columns_section-fourth" title="section3" id="section3">
+        <div className="columns_section-content">
+            <div className="about_title"><h1> O nas</h1>
+                <div className="about_decoration-image"> <img src ={Decoration} alt="decoration"/></div>
+                <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                <img src ={Signature} alt="signature"/>
+            </div>
+        </div>
+        <div className="about_image"><img src ={People} alt="people"/></div>
+    </div>
 );
 export {HomeAboutUs}
