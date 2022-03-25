e563e29703df763a85054a58e46bda37fd9ed672
Maksym Kaczorowski <maksym73@gmail.com>
2022-03-25 16:19:57 +0100

    Home About Us section Styling complete

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -11,7 +11,7 @@ const HomeAboutUs = () => (
             <div className="about_title"><h1> O nas</h1>
                 <div className="about_decoration-image"> <img src ={Decoration} alt="decoration"/></div>
                 <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
-                <img src ={Signature} alt="signature"/>
+                <img className="signature" src ={Signature} alt="signature"/>
             </div>
         </div>
         <div className="about_image"><img src ={People} alt="people"/></div>
