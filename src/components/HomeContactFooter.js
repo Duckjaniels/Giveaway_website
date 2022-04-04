acddfcb9924c3cd7c2780cff13c4e39d8376dfe0
Maksym Kaczorowski <maksym73@gmail.com>
2022-04-04 12:15:56 +0200

    Footer section styling

--- src/components/HomeContactFooter.js
+++ src/components/HomeContactFooter.js
@@ -18,17 +18,17 @@ const HomeContactFooter = () => (
              <div className="footer_form-info">
                  <label>
                      Podaj swoje imię
-                 <input className="input-100" name="name" required type="text"  />
+                 <input className="input-100" name="name" required type="text" placeholder="Krzysztof" />
                  </label>
                  <label>
                  Wpisz swój email
-                 <input className="input-100" name="email" required type="text" />
+                 <input className="input-100" name="email" required type="text" placeholder="abc@xyz.pl"/>
                  </label>
              </div>
                  <div className="footer_form-message">
                      <label>
                     Wpisz swoją wiadomość
-                  <textarea className="input-250" name="message" required  />
+                  <textarea className="input-250" name="message" required placeholder="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci animi aperiam aut eligendi fuga illo quos ratione rem, similique. Atque ea earum ipsam maxime, nemo non omnis pariatur repellat?"  />
                      </label>
                  </div>
                  <div className="footer_form-button"><button type="submit"  value="Submit">Wyślij</button></div>
