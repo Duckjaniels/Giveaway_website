eddb9dd53c272aba0ec95e3f20b958da61ca277a
Maksym Kaczorowski <maksym73@gmail.com>
2022-04-07 13:50:05 +0200

    Login section structure

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,52 @@
 import React from "react";
+import {Navbar} from "./Navbar";
+import Decoration from "../assets/Decoration.svg";
+import useForm from "../../src/components/useForm";
+import validate from "../../src/components/validateInfo"
+import {Link} from "react-router-dom";
+import "../scss/Login.scss"
 
-const Login = () => (
-    <div> Login screen</div>
-)
+
+const Login = () => {
+const {handleChange, values, handleSubmit, errors,} = useForm(validate);
+
+return(
+    <div className="login_section" title="section6" id="section6">
+        <div className="header_navbar">
+            <Navbar/>
+        </div>
+        <div className="login_title"><h1>Zaloguj się</h1>
+            <div className="login_decoration-image"> <img src ={Decoration} alt="decoration"/></div>
+        </div>
+
+        <form action="" className="login_form" onSubmit={handleSubmit}>
+            <div className="login_form-info">
+                <label>
+                    Email
+                    <input className="input-100"
+                           name="email"
+                           required type="text"
+                           value={values.email}
+                           onChange={handleChange}/>
+                    {errors.email && <p>{errors.email}</p>}
+                </label>
+                <label>
+                    Hasło
+                    <input className="input-100"
+                           name="password"
+                           required type="password"
+                           value={values.password}
+                           onChange={handleChange}/>
+                    {errors.password && <p>{errors.password}</p>}
+                </label>
+            </div>
+        </form>
+        <div className="login_user-panel">
+            <ul className="login_user-buttons">
+                <li className="login_user-register"> <Link to={"/Register"}> Załóż konto </Link></li>
+                <li className="login_user-login"> <Link to={"/Login"}>Zaloguj się </Link></li>
+            </ul>
+        </div>
+    </div>
+)};
 export {Login}
